bc4cc0458a2e724b04cddcf36007095818e8814a
hafiz011 <hrhafij8@gmail.com>
2025-05-24 12:43:33 +0600

    admin controller documentation done

--- obj/Debug/net8.0/ActivityTracker.AssemblyInfo.cs
+++ obj/Debug/net8.0/ActivityTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActivityTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0acd70f58f5f1c6af6ab99c86163ac7f696bc53c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6545d3d31613ddc12b2118c7c6f61f1652ad9bcd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActivityTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActivityTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
